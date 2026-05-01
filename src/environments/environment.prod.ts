f80ff4c75f4c05b739d41bc07053df8ab13833be
Ayush Rawat <ayushrawat@Ayushs-MacBook-Pro.local>
2026-05-01 22:40:22 +0530

    20 phase

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+      apiKey: "AIzaSyBayA_0DFFCnWlZdXJA1e02iMmOcHTNuLM",
+      authDomain: "speech-and-hearing-management.firebaseapp.com",
+      projectId: "speech-and-hearing-management",
+      storageBucket: "speech-and-hearing-management.firebasestorage.app",
+      messagingSenderId: "884676352040",
+      appId: "1:884676352040:web:570731302324219ea73d1d"
+    }
+  };
