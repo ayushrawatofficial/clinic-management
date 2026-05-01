5c13b4a8e238131230f61c61ca0b9599618e83dc
Ayush Rawat <ayushrawat@Ayushs-MacBook-Pro.local>
2026-05-01 12:31:28 +0530

    19 phase

--- src/app/core/config/clinic.config.ts
+++ src/app/core/config/clinic.config.ts
@@ -3,7 +3,7 @@ export const CLINIC_CONFIG = {
   subjectLine:'Hearing Aids, Speech Theraphy, Audiomety (Pure Tone, Inpedance), BERA, OAE',
   logo: 'assets/logo.png',
   phone: '+91 9414124900, +91 8209141635',
-  email: 'clinic@email.com',
+  email: 'akshit.1496@gmail.com',
   address: 'U.I.T. Shop No. 24, MDM Hospital Road, Shastri Nagar, Jodhpur',
   specialist1:'Vinod Sharma',
   specialist1Desg:'(Speech Therapist)',
